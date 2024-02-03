36da8c5215ddf9b600a4f43afe24ac7ffdf302ea
Jevgenijs Jaunslavietis <jevgenijs.jaunslavietis@gmail.com>
2024-02-03 17:33:25 +0200

    Add PKCE login flow with spring auth server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './auth.config';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'service-provider-pkce-client';
+
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+
+    this.oauthService.events
+      .pipe(filter((e) => e.type === 'token_received'))
+      .subscribe((_) => this.oauthService.loadUserProfile());
+  }
+
+  authenticate() {
+    this.oauthService.initLoginFlow();
+  }
+
+  get userName(): string | null {
+    const claims = this.oauthService.getIdentityClaims();
+    if (!claims) return null;
+    return claims['given_name'];
+  }
+
+  get idToken(): string {
+    return this.oauthService.getIdToken();
+  }
+
+  get accessToken(): string {
+    return this.oauthService.getAccessToken();
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
 }
---
+++ src/app/auth.config.ts
@@ -0,0 +1,13 @@
+import { AuthConfig } from 'angular-oauth2-oidc';
+
+export const authCodeFlowConfig: AuthConfig = {
+  // Url of the Identity Provider
+  issuer: 'http://localhost:8080',
+  redirectUri: 'http://localhost:4200',
+  clientId: 'public-client',
+  responseType: 'code',
+  scope: 'openid profile',
+  showDebugInformation: true,
+  loginUrl: 'http://localhost:8080/login/oauth2/public-client',
+  oidc: true,
+};
