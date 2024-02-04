aa9f10be2c2bc895684f79efe06c85b4d8570656
Jevgenijs Jaunslavietis <jevgenijs.jaunslavietis@gmail.com>
2024-02-04 21:11:05 +0200

    Add call to resource server with obtained token from auth server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { authCodeFlowConfig } from './auth.config';
 import { filter } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ import { filter } from 'rxjs';
 export class AppComponent {
   title = 'service-provider-pkce-client';
 
-  constructor(private oauthService: OAuthService) {
+  constructor(private oauthService: OAuthService, private http: HttpClient) {
     this.oauthService.configure(authCodeFlowConfig);
     this.oauthService.loadDiscoveryDocumentAndTryLogin();
 
@@ -26,7 +27,9 @@ export class AppComponent {
 
   get userName(): string | null {
     const claims = this.oauthService.getIdentityClaims();
-    if (!claims) return null;
+    if (!claims) {
+      return null;
+    }
     return claims['given_name'];
   }
 
@@ -40,5 +43,15 @@ export class AppComponent {
 
   logout() {
     this.oauthService.logOut();
+  }
+
+  callApi() {
+    this.http.get('http://localhost:8081/foos', {
+      headers: {
+        Authorization: 'Bearer ' + this.accessToken
+      }
+    }).subscribe((data) => {
+      console.log(data);
+    });
   }
 }
--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -6,7 +6,7 @@ export const authCodeFlowConfig: AuthConfig = {
   redirectUri: 'http://localhost:4200',
   clientId: 'public-client',
   responseType: 'code',
-  scope: 'openid profile',
+  scope: 'openid profile read write',
   showDebugInformation: true,
   loginUrl: 'http://localhost:8080/login/oauth2/public-client',
   oidc: true,
